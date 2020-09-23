45bfd0d1fccd9707a8ec5d3bbe8aef433aedf3ad
Luna <dlramos194@gmail.com>
2020-09-23 13:40:43 -0500

    restructured server

--- server.js
+++ server.js
@@ -1,70 +1,33 @@
-// Dependencies
-// =============================================================
+//Dependencies
+const path = require("path")
 const express = require("express");
-const path = require("path");
 const fs = require("fs")
+const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes = require("./routes/htmlRoutes");
+// ==============================================================================
 
-//Link our files 
-//==========================================
-const db = require("./develop/db/db.json")
-const mainDir = path.join(__dirname, "/public");
-
-// Sets up the Express App
-// =============================================================
+// Setting the express server
 const app = express();
-const PORT = process.env.PORT || 8080;
+//========================================================================
+// Adding the port 
+const PORT = process.env.PORT || 8000;
 
-// Sets up the Express app to handle data parsing
-//=============================================================
+//==================================================================
+
+// Handling parsing data
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(__dirname + "/public"))
-    //==========================================================
-
-// SET PARAMETERS:
-//======================================================
+app.use(express.static("public"))
 
 
-// ROUTES:
-//=====================================
-//1.GET
-
-app.get("*", function(req, res) {
-    res.sendFile(path.join(mainDir, "index.html"));
-
-});
-
-app.get("/notes", function(req, res) {
-    res.sendFile(path.join(mainDir, "notes.html"));
+// ================================================================================
+// ROUTES
 
 
-});
 
-app.get("/api/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "./develop/db/db.json"))
-        //return all saved notes
-
-})
-
-app.get("/api/notes/:id", function(req, res) {
-    res.sendFile(path.join(__dirname, ""))
-})
-
-//2.POST
-
-app.post("/api/notes", function(req, res) {
-
-});
-
-//3.DELETE 
-
-app.delete("/api/notes/:id", function(req, res) {
-    var id = req.params.id
-});
-
-//listen:
-//======================================================
+// =============================================================================
+// LISTENER 
 
 app.listen(PORT, function() {
-    console.log("App port listening on PORT " + PORT)
+    console.log("App listening on PORT: " + PORT);
 });
